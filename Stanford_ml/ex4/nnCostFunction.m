2e336a4cb328df804398ed4e581e6bb58ffbbbab
GoingMyWay <scutqiuwei@163.com>
2015-08-11 21:58:40 +0800

    Regularized Gradient

--- Stanford_ml/ex4/nnCostFunction.m
+++ Stanford_ml/ex4/nnCostFunction.m
@@ -112,6 +112,9 @@ end
 Theta2_grad = Delta2 / m;
 Theta1_grad = Delta1 / m;
 
+Theta1_grad( :,2:end) = Theta1_grad( :,2:end) + (lambda/m) * Theta1( :,2:end);
+Theta2_grad( :,2:end) = Theta2_grad( :,2:end) + (lambda/m) * Theta2( :,2:end); 
+
 % -------------------------------------------------------------
 
 % =========================================================================
