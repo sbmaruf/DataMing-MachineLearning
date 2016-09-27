1c4119a0662429c9b31c5770021b87e93cc87418
GoingMyWay <scutqiuwei@163.com>
2016-09-27 23:56:28 +0800

    tree.py

--- EnsembleLearning/Tree/tree.py
+++ EnsembleLearning/Tree/tree.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding=utf-8
 import sys
+import math
+
 sys.path.append('..')
 from utils import utils
 
@@ -9,8 +11,8 @@ class Tree(object):
     """
     树的父类
     """
-    def __init__(self):
-        pass
+    def __init__(self, data_set):
+        self.data_set = data_set
 
     def train(self):
         pass
@@ -18,8 +20,29 @@ class Tree(object):
     def prune(self):
         pass
 
+    def get_entropy(self):
+        _num_entries = len(self.data_set)
+        _label_counts = dict()
+        for vec in self.data_set:
+            _curr_label = vec[-1]
+            _label_counts[_curr_label] = _label_counts.get(_curr_label, 0) + 1
+        entropy = 0
+        for key in _label_counts.keys():
+            _prob = float(_label_counts[key]) / _num_entries
+            entropy -= _prob * math.log(_prob, 2)
+        return entropy
 
-class TreeNode(object):
+    def split_data(self, split_data, axis, value):
+        ret_data = []
+        for _vec in split_data:
+            if _vec[axis] == value:
+                _reduced_feature_vec = _vec[:axis]
+                _reduced_feature_vec.extend(_vec[axis+1:])
+                ret_data.append(_reduced_feature_vec)
+        return ret_data
+
+
+class TreeNode(Tree):
     """
     决策树中的节点
     """
@@ -31,6 +54,8 @@ class TreeNode(object):
         self.r_child = right_child
         utils.check_type(is_leaf, bool)
         self.leaf = is_leaf
+
+        super(self, TreeNode).__init__()
 
 
 class RegressionTee(Tree):
@@ -45,3 +70,10 @@ class ClassifierTree(Tree):
     分类树
     """
     pass
+
+
+def create_data_set():
+    data_set = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return data_set, labels
+
